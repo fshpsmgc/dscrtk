fcfd981dd4114646cc7842c4d8534dc241fe52eb
Diaglyonok <Diaglyonok@yandex.ru>
2016-04-19 22:48:57 +0300

    Модуль для вычисления абсолютного значения
    Диденко Олег

---
+++ DIV_NN_N.cpp
@@ -0,0 +1,29 @@
+//VK: http://new.vk.com/diaglyonok
+//E-Mail: diaglyonok@yandex.ru
+
+
+DM_N DIV_NN_N (DM_N X1, DM_N X2)
+{
+   else
+      {
+        do
+         {
+         }
+      }
+
+      else
+           {
+                xCopy = X2;
+                do
+                  {
+                    xCopy=SUB_NN_N(xCopy,X1);
+                    t=ADD_1N_N(t);
+                  }
+                while (COM_NN_D(xCopy, X1)==2);
+                return t;
+           }
+        else
+          {
+          }
+}
+
