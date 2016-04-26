cde5b5247a0a237b6a216740bfa31373b22e9693
Nikita <mqmpgl@gmail.com>
2016-04-26 17:00:40 +0300

    Replaced inferior realization

--- MOD_NN_N.cpp
+++ MOD_NN_N.cpp
@@ -1,6 +1,7 @@
-//Вялков Дмитрий
-//Остаток от делени¤ натуральных чисел натуральных чисел
-DM_N MOD_NN_N(n1, n2) {
-	return SUB_NN_N(n1, MUL_NN_N(n2, DIV_NN_N(n1, n2)))
+DM_N DM_N::MOD_NN_N(DM_N op1, DM_N op2) {
+    int N;
+    do {
+    N++;
+    } while(!(op1->a = N*(op2->a)));
+    return(SUB_NN_N(op1->a, (N-1)* DIV_NN_N(op1->a, op2->a)));
 }
-//не тестил
