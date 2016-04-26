8a9c7bf3ada579abbf3f9f83a3086eb0e78f2631
fshpsmgc <fshpsmgc@gmail.com>
2016-04-27 01:19:45 +0300

    Updated module

--- INT_Q_Z.cpp
+++ INT_Q_Z.cpp
@@ -4,9 +4,10 @@
 */
 
 bool INT_Q_Z(DM_Q n1){
-  if(n1.b.n == 1 && n1.b.a[0] == 1){
-    return true;
-  }else{
-    return false;
-  }
+	REQ_Q_Q(n1);
+	if(n1.b.n == 1 && n1.b.a[0] == 1){
+	  return true;
+	}else{
+	  return false;
+	}
 }
