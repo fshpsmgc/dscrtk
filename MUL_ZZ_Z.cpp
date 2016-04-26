6232ffa3e27843866b0efeb1f64cc77d845be2be
vyalich <dimvyalkov97@gmail.com>
2016-04-27 00:37:36 +0300

    Добавил комментарии и автора

--- MUL_ZZ_Z.cpp
+++ MUL_ZZ_Z.cpp
@@ -1,15 +1,18 @@
-DM_Z MUL_QQ_Q(DM_Z n1, DM_Z n2){
-	DM_N n1abs;
-	DM_N n2abs;
-	DM_N natres;
-	DM_Z res;
+Автор: Вялков Дмитрий
+Задача модуля: умноженние целых чисел
+
+DM_Z MUL_ZZ_Z(DM_Z n1, DM_Z n2){
+	DM_N n1abs;					//модуль первого числа
+	DM_N n2abs;					//модуль второго числа
+	DM_N natres;				//модуль произведения
+	DM_Z res;					//произвеление
 	n1abs = ABS_Z_N(n1);
 	n2abs = ABS_Z_N(n2);
 	natres = MUL_NN_N(n1, n2);
-	res = TRANS_N_Z(natres);
-	if(n1.b == n2.b){
+	res = TRANS_N_Z(natres);	
+	if(n1.b == n2.b){			//если знаки чисел равны то выводим произвеление
 		return res;
-	}else{
-		return MUL_ZM_Z(res)
+	}else{						//если знаки чисел не равны то умножаем произведение на -1
+		return MUL_ZM_Z(res)	
 	}	
 }
