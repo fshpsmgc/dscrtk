4ae88c2526ab6a83f9291698e45ddf26ede13280
SirSterben <myxa20124@gmail.com>
2016-04-27 00:11:57 +0400

    .

--- LED_P_Z.cpp
+++ LED_P_Z.cpp
@@ -1,9 +1,7 @@
 //Тазеруалет М.А. гр. 5306
 //Функция взятия коэффициента при старшей степени
 
-DM_P LED_P_Z(DM_Q *c, unsigned short m)
+DM_Q LED_P_Q(DM_P obj)
 {
-	DM_Q f;
-	f = c[m];
-	return f;
+	return obj.c[obj.m];	//Возвразщаем сразу
 }
--- MUL_Pxk_P.cpp
+++ MUL_Pxk_P.cpp
@@ -1,9 +1,20 @@
 //Тазеруалет М.А. гр.5306
 //Умножение многочлена на x^k 
 
-DM_P MUL_Pxk_P(DM_Q *c, unsigned short m, unsigned short k, unsigned short x)
+//Функция для умножения многочлена на x^k
+DM_P MUL_Pxk_P(DM_P obj, unsigned short k)
 {
-	for (int i = 0; i < m; i++)
-		*(c + i) = (*(c + i)) * x^k;
-	return *c;
+	DM_P *result;		//Создаем новый
+	result = new DM_P;
+
+	result->m = obj.m + k;				//Его степень увеличится
+	result->c = new DM_Q[result->m + 2];
+
+	for (short i = 0; i < k; ++i)		//Забиваем нулями новые члены
+		result->c[i] = { 0, 1 };
+
+	for (short i = 0; i < obj.m + 1; ++i)	//Переносим старые
+		result->c[i + k] = obj.c[i];
+
+	return *result;
 }
